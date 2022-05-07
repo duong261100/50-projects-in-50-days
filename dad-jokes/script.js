988a4900e7dfb9332b5d760ab37a450fd047e71a
vubinhduong <duong261100@gmail.com>
2022-05-07 18:42:40 +0700

    10. dad-jokes

---
+++ dad-jokes/script.js
@@ -0,0 +1,20 @@
+const jokeBtn = document.querySelector('#jokeBtn')
+const jokeField = document.querySelector('#joke')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  const res = await fetch('https://icanhazdadjoke.com', config)
+
+  const data = await res.json()
+
+  jokeField.innerHTML = data.joke
+}
